81e53dd3521ab6bb03c1784b4927e75bc5704d4d
Documenter.jl <documenter@juliadocs.github.io>
2025-08-22 04:29:42 +0000

    build based on 28e6318

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#InformationTheory","page":"Home","title":"InformationTheory","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for InformationTheory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
